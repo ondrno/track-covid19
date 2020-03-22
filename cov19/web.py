0c628959f7325198cd4b0c672b432dda374a893a
Sven Hessler <sven@xafilac.de>
2020-03-22 21:37:00 +0100

    use callbacks to read data every two hours

--- cov19/web.py
+++ cov19/web.py
@@ -1,26 +1,26 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import flask
 import pandas as pd
+from datetime import datetime
 
 
-def read_data(logfile: str = "../log/cov19_statistics.log"):
-    df = pd.read_csv(logfile, sep=";")
-    df.drop_duplicates(keep='last', subset=['country', 'cases', 'deaths', 'recovered'], inplace=True)
-
-    grouped = df.groupby('country')
-    de = grouped.get_group('DE')
-    at = grouped.get_group('AT')
-    ch = grouped.get_group('CH')
-    uk = grouped.get_group('UK')
-    us = grouped.get_group('US')
-    return de, at, ch, uk, us
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+app.config.suppress_callback_exceptions = True
 
 
 def serve_layout():
-    de, at, ch, uk, us = read_data()
+    # two_hours_in_ms = 2 * 3600 * 1000
+    two_hours_in_ms = 60 * 1000
     return html.Div(children=[
+        dcc.Interval(
+            id='interval-component',
+            interval=two_hours_in_ms,
+            n_intervals=0
+        ),
         html.H1(children='Covid-19 Statistics (D, A, CH, UK, US)'),
 
         html.Div(children=[
@@ -40,47 +40,75 @@ def serve_layout():
             dcc.Link('Centers for Disease Control and Prevention (US)',
                      href='https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html?CDC_AA_refVal=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Fcases-in-us.html'),
             html.Br(),
+            html.Div(id='last-updated')
         ]),
-
-        dcc.Graph(
-            id='covid-cases',
-            figure={
-                'data': [
-                    {'x': at["dt"], 'y': at["cases"], 'name': "Austria"},
-                    {'x': de["dt"], 'y': de["cases"], 'name': "Germany"},
-                    {'x': ch["dt"], 'y': ch["cases"], 'name': "Switzerland"},
-                    {'x': uk["dt"], 'y': uk["cases"], 'name': "United Kingdom"},
-                    {'x': us["dt"], 'y': us["cases"], 'name': "United States"},
-                ],
-                'layout': {
-                    'title': 'Confirmed cases'
-                }
-            }
-        ),
-
-        dcc.Graph(
-            id='covid-deaths',
-            figure={
-                'data': [
-                    {'x': at["dt"], 'y': at["deaths"], 'name': "Austria"},
-                    {'x': de["dt"], 'y': de["deaths"], 'name': "Germany"},
-                    {'x': ch["dt"], 'y': ch["deaths"], 'name': "Switzerland"},
-                    {'x': uk["dt"], 'y': uk["deaths"], 'name': "United Kingdom"},
-                    {'x': us["dt"], 'y': us["deaths"], 'name': "United States"},
-                ],
-                'layout': {
-                    'title': 'Confirmed deaths'
-                }
-            }
-        )
+        dcc.Graph(id='covid-cases-graph'),
+        dcc.Graph(id='covid-deaths-graph'),
     ])
 
 
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
-app.config.suppress_callback_exceptions = True
+def read_data(logfile: str = "../log/cov19_statistics.log"):
+    df = pd.read_csv(logfile, sep=";")
+    df.drop_duplicates(keep='last', subset=['country', 'cases', 'deaths', 'recovered'], inplace=True)
+    grouped = df.groupby('country')
+    de = grouped.get_group('DE')
+    at = grouped.get_group('AT')
+    ch = grouped.get_group('CH')
+    uk = grouped.get_group('UK')
+    us = grouped.get_group('US')
+    return de, at, ch, uk, us
+
+
+@app.callback(Output('last-updated', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_metrics_text(n):
+    today = datetime.now().isoformat()
+    style = {'fontSize': '11px'}
+    return [
+        html.Span('Last update: {}'.format(today), style=style)
+    ]
+
+
+@app.callback(Output('covid-cases-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_covid_cases_metrics(n):
+    de, at, ch, uk, us = read_data()
+    fig={
+        'data': [
+            {'x': at["dt"], 'y': at["cases"], 'name': "Austria"},
+            {'x': de["dt"], 'y': de["cases"], 'name': "Germany"},
+            {'x': ch["dt"], 'y': ch["cases"], 'name': "Switzerland"},
+            {'x': uk["dt"], 'y': uk["cases"], 'name': "United Kingdom"},
+            {'x': us["dt"], 'y': us["cases"], 'name': "United States"},
+        ],
+        'layout': {
+            'title': 'Confirmed cases'
+        }
+    }
+    return fig
+
+
+@app.callback(Output('covid-deaths-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_covid_cases_metrics(n):
+    de, at, ch, uk, us = read_data()
+    fig = {
+        'data': [
+            {'x': at["dt"], 'y': at["deaths"], 'name': "Austria"},
+            {'x': de["dt"], 'y': de["deaths"], 'name': "Germany"},
+            {'x': ch["dt"], 'y': ch["deaths"], 'name': "Switzerland"},
+            {'x': uk["dt"], 'y': uk["deaths"], 'name': "United Kingdom"},
+            {'x': us["dt"], 'y': us["deaths"], 'name': "United States"},
+        ],
+        'layout': {
+            'title': 'Confirmed deaths'
+        }
+    }
+    return fig
+
 app.layout = serve_layout
 
+
 if __name__ == '__main__':
-    df = read_data()
+    # df = read_data()
     app.run_server(port=8050)
