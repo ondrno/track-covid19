6a3aedd33d2f0912c7d4117f3df283646052647e
Sven Hessler <sven@xafilac.de>
2020-03-29 18:03:39 +0200

    extend unit tests

--- tests/collect/test_us.py
+++ tests/collect/test_us.py
@@ -1,4 +1,4 @@
-import pytest
+import mock
 import json
 import responses
 import pathlib
@@ -9,7 +9,7 @@ base_path = pathlib.Path(__file__).parent.parent
 
 
 @responses.activate
-def test_get_data_returns_values():
+def test_get_cov19_data_returns_values():
     us = UnitedStates()
     with open("{}/res/us_fallzahlen.html".format(base_path)) as f:
         body = f.read()
@@ -17,3 +17,14 @@ def test_get_data_returns_values():
         data = us.get_cov19_data()
 
         assert data == json.dumps({"country": "US", 'c': 85356, 'd': 1246})
+
+
+@mock.patch('cov19.collect.UnitedStates.check_data')
+@responses.activate
+def test_get_cov19_data_returns_empty_list_if_no_li_found(mock_check):
+    us = UnitedStates()
+    body = "<html></html>"
+    responses.add(responses.GET, us.url, body=body, status=200)
+    data = us.get_cov19_data()
+    assert data == json.dumps({})
+    mock_check.assert_called()
