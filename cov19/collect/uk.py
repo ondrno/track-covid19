2f85e4a610d2d675e2a4e703fbca7ea62b966765
Sven Hessler <sven@xafilac.de>
2020-04-14 23:20:18 +0200

    fix uk tracker as of 14.04.2020
    - use different tracker url for UK

--- cov19/collect/uk.py
+++ cov19/collect/uk.py
@@ -1,21 +1,40 @@
 import pandas as pd
+import requests
+from loguru import logger
+import cov19
 from cov19.collect import DataCollector
 
 
 class UnitedKingdom(DataCollector):
     def __init__(self):
         super().__init__("United Kingdom")
-        self.url = "https://www.arcgis.com/sharing/rest/content/items/bc8ee90225644ef7a6f4dd1b13ea1d67/data"
+        self.url = "https://www.gov.uk/guidance/coronavirus-covid-19-information-for-the-public"
 
     def get_cov19_data(self):
         try:
-            tables = pd.read_excel(self.url)
-            cases = tables.iloc[0]['TotalUKCases']
-            deaths = tables.iloc[0]['TotalUKDeaths']
-            self.data['c'] = int(cases)
-            self.data['d'] = int(deaths)
-        except KeyError:
-            pass
-
+            response = requests.get(self.url)
+        except (requests.exceptions.HTTPError,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.Timeout):
+            raise cov19.exceptions.RequestError(self.url)
+        if response.status_code == 200:
+            try:
+                tables = pd.read_html(response.text, thousands=",", decimal=";")
+                self.get_total_cases(tables[0])
+                self.get_total_deaths(tables[0])
+            except ValueError:
+                pass
+        else:
+            logger.error("Could not access statistics for United Kingdom (status code={})".format(response.status_code))
         self.check_data()
         return self.get_data_as_json()
+
+    def get_total_cases(self, table):
+        cases = table['Positive'][1]
+        cases = int(cases)
+        self.data['c'] = cases
+
+    def get_total_deaths(self, table):
+        deaths = table['Deaths'][1]
+        deaths = int(deaths)
+        self.data['d'] = deaths
