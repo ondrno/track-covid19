5e69f15ce4008d1160cb2786d1fdfe887f6b4a08
Sven Hessler <sven@xafilac.de>
2020-04-17 23:28:06 +0200

    fix Austrian tracker as of 16.04.2020

--- tests/collect/test_at.py
+++ tests/collect/test_at.py
@@ -26,38 +26,13 @@ def test_get_cov19_data_returns_values(at: Austria):
 
         assert data['country'] == "AT"
         assert 'c' in data, "Could not find 'cases'"
-        assert data['c'] >= 13561, 'Invalid number for cases in AT'.format(13561, data['c'])
+        assert data['c'] >= 13561, 'Invalid number for cases in AT'.format(14451, data['c'])
 
         assert 'd' in data, "Could not find 'deaths'"
-        assert data['d'] >= 337, 'Invalid number for deaths in AT'.format(337, data['d'])
+        assert data['d'] >= 337, 'Invalid number for deaths in AT'.format(410, data['d'])
 
         assert_if_provinces_have_no_cases_and_deaths(at, data)
         assert_if_province_data_is_equal(at, data)
-
-
-def test_get_re_pattern_for_province_invalid_what(at):
-    with pytest.raises(ValueError):
-        at._get_re_pattern_for_province('f', 'Burgenland')
-
-
-def test_get_re_pattern_for_province_invalid_province(at):
-    with pytest.raises(ValueError):
-        at._get_re_pattern_for_province('c', 'WienerLand')
-
-
-@pytest.mark.parametrize("what, number", [('c', '42'), ('d', '42')])
-@mock.patch('cov19.collect.Austria._get_re_pattern_for_province')
-@mock.patch('cov19.collect.Austria._str2int')
-def test_get_numbers_by_province_for_burgenland_if_match(mock_str2int, mock_re, what, number, at):
-    string = "foo - {}".format(number)
-    mock_str2int.return_value = 10
-    mock_re.return_value = r'(foo) - (\d+)'
-    at.provinces = {'Burgenland': {'short_name': 'BL', 'search_pattern': r'Burgenland'}}
-
-    at._get_numbers_by_province(what, string)
-    assert at.data['provinces']['BL'][what] == 10
-    mock_str2int.assert_called_once_with(number)
-    mock_re.assert_called_once_with(what, 'Burgenland')
 
 
 @mock.patch('cov19.collect.Austria._get_numbers_by_province')
@@ -72,15 +47,15 @@ def test_get_cases_by_province_calls_correct_func(mock_get, at):
     mock_get.assert_called_once_with('c', 'cases')
 
 
-@mock.patch('cov19.collect.Austria._get_total_numbers')
-def test_get_total_cases_calls_correct_func(mock_get, at):
-    at.get_total_cases("total_cases")
-    mock_get.assert_called_once_with('c', 'total_cases')
+@mock.patch('cov19.collect.Austria.get_cases_by_province')
+@mock.patch('cov19.collect.Austria.get_deaths_by_province')
+@mock.patch('pandas.DataFrame')
+def test_get_all_cases_calls_correct_funcs(mock_df, mock_d, mock_c, at):
+    mock_iloc = mock.MagicMock()
+    mock_df.filter.return_value = mock_iloc
 
+    at.get_all_cases(mock_df)
 
-@mock.patch('cov19.collect.Austria._get_total_numbers')
-def test_get_total_deaths_calls_correct_func(mock_get, at):
-    at.get_total_deaths("total_deaths")
-    mock_get.assert_called_once_with('d', 'total_deaths')
-
-
+    mock_df.filter.assert_called_once_with(regex=at.total_pattern)
+    mock_c.assert_called_once_with(mock_df)
+    mock_d.assert_called_once_with(mock_df)
